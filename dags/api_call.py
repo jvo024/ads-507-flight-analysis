2f1e35c2aa7d2bf3b9b7d7afcc40c2316f350bf3
jvo024 <jvo@sandiego.edu>
2024-02-12 14:26:18 -0800

    added to raw datasets

--- dags/api_call.py
+++ dags/api_call.py
@@ -1,121 +1,75 @@
 import json
+import os
 import requests
-
 import datetime
 from datetime import timedelta
 
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
-from azure.identity import DefaultAzureCredential
-from azure.mgmt.resource import ResourceManagementClient
+from dotenv import load_dotenv
+load_dotenv()
 
-import airflow
-from airflow.models import DAG, BaseOperator
-from airflow.operators.python import PythonOperator
+def operator_risk_call(api_key, file_name):
 
-from airflow.utils.edgemodifier import Label
-
-
-# Credentials for Azure
-
-azure_connection_id = 'azure-data-factory'
-azure_connection_string = 'DefaultEndpointsProtocol=https;AccountName=airplaneetl;AccountKey=K0pHJK8paR2yKLvxJKbz9nk7Fe4qsfsgMc/jZLYLtsXr+OTtyPVs8QcGYufvts3tJO2JYjRWKqWi+ASt71ofQw==;EndpointSuffix=core.windows.net'
-container_name = 'airplanec'
-factory_name = 'airplane-df'
-resource_group_name = 'ads507-team6'
-
-# Python Operator for ICAO API Call
-
-icao_api_key = 'a74b3990-66e7-44f8-ad40-0dc664d3912a'
-icao_state_of_operator = 'USA'
-icao_file_name = 'icao_api_scrape.json'
-
-def icao_api_call(api_key, state_of_operator, file_name, container_name, azure_connection_string):
-    print('Start ICAO API call')
+    print('Start API call')
     
-    url = f"https://applications.icao.int/dataservices/api/any?api_key={api_key}&StateOfOccurrence=&format=json&StateOfOperator={state_of_operator}&StateOfRegistry=&Year="
+    url = f"https://applications.icao.int/dataservices/api/profile-stats?api_key={api_key}&states=usa&format=json&operators="
 
     # Make the API call
     response = requests.get(url)
 
-    # Check if the request was successful (status code 200)
+    # Check if the request was successful
     if response.status_code == 200:
         json_data = response.json()
 
-        # Save the JSON data to a file
-        with open(file_name, 'w', encoding='utf') as json_file:
-            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
-        
-        # Upload the file to Azure Storage
-        blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-        container_client = blob_service_client.get_container_client(container_name)
-        with open(file_name, "rb") as data:
-            container_client.upload_blob(name=file_name, data=data)
+        # Define the directory path
+        directory = os.path.join(os.getcwd(), 'raw_data')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-        print('End ICAO API call')
+        file_path = os.path.join(directory, file_name)
+
+        # Save the JSON data to the file path
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+        print('End API call')
 
     else:
         print(f"Error: Unable to fetch data. Status code: {response.status_code}")
 
-# Function for Open Sky API Call
-        
+def accidents_call(api_key, file_name):
 
-# Function for Kaggle API Call
-        
-
-# Function for Tomorrow.io API Call
-
-
-# DAGs  
-# start_date = datetime(2024, 2, 7)
-
-# default_args = {
-#     'owner': 'airflow',
-#     'depends_on_past': False,
-#     'email_on_failure': False,
-#     'email_on_retry': False,
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5),  
-# }
-
-icao_api_call(icao_api_key, icao_state_of_operator, icao_file_name, container_name, azure_connection_string)
-
-# with DAG(
-#     dag_id="api_call_pipeline",
-#     start_date=start_date,
-#     schedule_interval="@weekly",
-#     catchup=False,
-#     default_args=default_args,  
-#     default_view="graph",
-# ) as dag:
-
-#     begin = EmptyOperator(task_id="begin")
-#     end = EmptyOperator(task_id="end")
-
-#     icao_task = PythonOperator(
-#         task_id='icao_api_call',
-#         python_callable=icao_api_call,
-#         op_kwargs={
-#             'api_key': icao_api_key,
-#             'state_of_operator': icao_state_of_operator,
-#             'file_name': icao_file_name,
-#             'container_name': container_name,
-#             'azure_connection_string': azure_connection_string
-#         },
-#         dag=dag  
-#     )
+    print('Start API call')
     
-#     run_pipeline_operator: BaseOperator = AzureDataFactoryRunPipelineOperator(
-#         task_id="run_adf_pipeline",
-#         pipeline_name="pipeline",
-#         azure_data_factory_conn_id=azure_connection_id,
-#         resource_group_name=resource_group_name,
-#         factory_name=factory_name,
-#         params={"param1": "{{ task_instance.xcom_pull(task_ids='icao_api_call', key='file_name') }}"}  
-#     )
+    url = f"https://applications.icao.int/dataservices/api/accidents?api_key={api_key}&StateOfOccurrence=&format=json&StateOfOperator=usa&StateOfRegistry=&Year="
 
-#     pipeline_run_sensor: BaseOperator = AzureDataFactoryPipelineRunStatusSensor(
-#         task_id="pipeline_run_sensor",
-#         run_id=run_pipeline_operator.output["run_id"]
-#     )
-    
-#     begin  >> icao_task >> run_pipeline_operator >> pipeline_run_sensor >> end
+    # Make the API call
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        json_data = response.json()
+
+        # Define the directory path
+        directory = os.path.join(os.getcwd(), 'raw_data')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file_path = os.path.join(directory, file_name)
+
+        # Save the JSON data to the file path
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+        print('End API call')
+
+    else:
+        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+
+# Variables        
+icao_api_key = os.getenv("icao_api_key")
+
+operator_risk_file_name = 'airline_operator_risk.json'
+accidents_file_name = 'airline_accidents.json'
+
+operator_risk_call(icao_api_key, operator_risk_file_name)
+accidents_call(icao_api_key, accidents_file_name)
--- dags/kaggle_download.py
+++ dags/kaggle_download.py
@@ -1,39 +1,34 @@
 import os
 from datetime import timedelta
+import pandas as pd
 
 from azure.storage.blob import BlobServiceClient
 import opendatasets as od
 
-import airflow
-from airflow.models import DAG
-from airflow.operators.python import PythonOperator
-
-# Credentials for Azure
-
-azure_connection_string = 'DefaultEndpointsProtocol=https;AccountName=airplaneetl;AccountKey=K0pHJK8paR2yKLvxJKbz9nk7Fe4qsfsgMc/jZLYLtsXr+OTtyPVs8QcGYufvts3tJO2JYjRWKqWi+ASt71ofQw==;EndpointSuffix=core.windows.net'
-container_name = 'airplanec'
-
-def kaggle_download(dataset_url, file_name, azure_connection_string, container_name):
+def kaggle_download(dataset_url, file_name):
 
     print('Start Kaggle CSV download')
     
     # Download dataset using opendatasets
     od.download(dataset_url, force=True, quiet=False)
     
-    # Upload the file to Azure Storage
-    blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
+    # Define the directory path
+    directory = os.path.join(os.getcwd(), 'raw_data')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_path = os.path.join(directory, file_name)
     
-    # Upload airports.csv
-    with open(file_name, "rb") as data:
-        container_client.upload_blob(name=os.path.basename(file_name), data=data)
+    # Read the CSV file
+    csv_data = pd.read_csv(file_path)
+
+    # Save the df to a new CSV
+    csv_data.to_csv(file_path, index=False)
 
     print('End Kaggle CSV download')
 
 # Variables for Kaggle Download
 dataset_url = 'https://www.kaggle.com/datasets/tylerx/flights-and-airports-data'
 file_name = 'flights-and-airports-data/airports.csv'
-azure_connection_string = azure_connection_string
-container_name = container_name
 
-kaggle_csv_download(dataset_url, file_name, azure_connection_string, container_name)
+kaggle_download(dataset_url, file_name)
